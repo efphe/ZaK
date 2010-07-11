1ea42b0ef42b82ae8ce3b631958ce4db81c98c8e
effe <effe@shiva.(none)>
2010-07-11 23:45:37 +0200

    Invoicing, need to change something

--- src/js/invoice.js
+++ src/js/invoice.js
@@ -15,19 +15,24 @@ $(document).ready(function() {
   var reservation= JSON.parse(localStorage.invoiceReservation);
   zakReservation= iReservation(reservation, 1);
 
-  var extras= JSON.parse(zakReservation.extras);
-  var res= '';
-  var etotal= 0.0;
-  for (var key in extras) {
-    var extra= extras[key];
-    res+= '<tr><td>' + extra['how'] + '</td><td>X</td>';
-    res+= '<td>' + extra['name'] + '</td>';
-    res+= '<td>' + extra['cost'] + '</td>';
-    res+= '</tr>';
-    etotal+= parseFloat(extra['cost']);
+  try {
+    var extras= JSON.parse(zakReservation.extras);
+    var res= '';
+    var etotal= 0.0;
+    for (var key in extras) {
+      var extra= extras[key];
+      res+= '<tr><td>' + extra['how'] + '</td><td>X</td>';
+      res+= '<td>' + extra['name'] + '</td>';
+      res+= '<td>' + extra['cost'] + '</td>';
+      res+= '</tr>';
+      etotal+= parseFloat(extra['cost']);
+    }
+    res+= '<tr><td colspan="3"><b>Total:</b></td><td>' + etotal.toFixed(2) + '</td></tr>';
+    $('#extraspricing').html(res);
+  } catch(e) {
+    etotal= 0.0;
   }
-  res+= '<tr><td colspan="3"><b>Total:</b></td><td>' + etotal.toFixed(2) + '</td></tr>';
-  $('#extraspricing').html(res);
+  $('#totextras').html(etotal.toFixed(2));
   $('#guest').html(zakReservation.reservation.customer);
   console.log(strDate(zakTableau.dfrom));
 
@@ -46,6 +51,7 @@ $(document).ready(function() {
         count+= parseFloat(rp[i]);
       }
     }
+    $('#totrooms').html(count.toFixed(2));
     count+= parseFloat(etotal);
     $('#itotal').html(count.toFixed(2) + ' &#8364');
     $('#stay').html('From ' + strDate(zakTableau.dfrom) + ' to ' + strDate(dateAddDays(zakTableau.dfrom, zakTableau.lendays)));
