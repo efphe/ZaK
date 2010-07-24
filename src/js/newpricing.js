8ae5dbd7bbd01c814a988169ca912e75b80e3feb
effe <effe@krishna.(none)>
2010-07-24 19:01:09 +0200

    pricing

--- src/js/newpricing.js
+++ src/js/newpricing.js
@@ -1,5 +1,19 @@
 _zakRtypes= [];
 
+function _decompressPricing(prices) {
+  try {
+    return JSON.parse(prices);
+  } catch(e) {return -1};
+}
+
+function _getRtPricing(prices, rt) {
+  try {
+    var res= parseFloat(prices[rt+'']);
+    if (!res) return 0.0;
+    return res.toFixed(2);
+  } catch(e) {return 0.0};
+}
+
 function designPricing() {
 
   llLoadPricings(function(ses, recs) {
@@ -15,25 +29,88 @@ function designPricing() {
     function(ses, recs) {
       console.log(recs);
       var p= recs.rows.item(0);
-      try {
-        var prices= JSON.parse(p.pricing);
-      } catch(e) {var prices= -1;};
+      var prices= _decompressPricing(p.prices);
+      console.log('Prices');
+      console.log(prices);
       var res= '';
 
       function _gp(rt) {
-        try {
-          return prices[rt] || 0.0;
-        } catch(e) {return 0.0};
+        return _getRtPricing(prices, rt);
       }
 
+      console.log('Now cycling rtypes');
       for (var i= 0; i< _zakRtypes.length; i++) {
-        var rtype= _zakRtypes[i];
-        res+= '<tr><td>' + rtype.name + '</td>';
+        var rtype= _zakRtypes[i + ''];
+        res+= '<tr><td><b>' + rtype.name.substring(0,6) + '</b></td>';
         res+= '<td><input type="text" id="price_' + rtype.id + '" style="width:75px" value="';
         res+= _gp(rtype.id) + '"></input>';
       }
       $('#defprices').empty().html(res);
     });
+
+  llLoadPricesPeriods(getActivePricing(),
+    function(ses, recs) {
+      var hres= '<th><b>From</b></th><th><b>To</b></th>';
+      var dres= '<td><input type="text" id="pdfrom" style="width:75px"></input></td>';
+      dres+= '<td><input type="text" id="pdto" style="width:75px"></input></td>';
+      for (var z= 0; z< _zakRtypes.length; z++ ) {
+        var zt= _zakRtypes[z];
+        hres+= '<th><b>' + zt.name.substring(0,6) + '</b></th>';
+        dres+= '<td><input type="text" id="pprice_' + zt.id + '" style="width:60px"></input></td>';
+      }
+      hres+= '<th><b>Save</b></th>';
+      dres+= '<td><input type="submit" value="Save"></td>';
+      $('#lrtypesname').empty().html(hres);
+      $('#lrtypesinput').empty().html(dres);
+
+      for (var j= 0; j< recs.rows.length; j++) {
+        var pp= recs.rows.item(i);
+        var pprices= _decompressPricing(pp.prices);
+        var ppres= '<tr><td>' + strDate(pp.dfrom) + '</td><td>' + strDate(pp.dto) + '</td>';
+        for (var z= 0; z< _zakRtypes.length; z++ ) {
+          ppres+= '<td>' + _getRtPricing(pprices, zt.id) + '</td>';
+        }
+        ppres+= '</tr>';
+      }
+      $('#defprices').empty().html(ppres);
+    });
+}
+
+function saveBasePrices() {
+  var prices= {};
+  for (var i= 0; i< _zakRtypes.length; i++) {
+    var rt= _zakRtypes[i];
+    var p= $('#price_' + rt.id).val();
+    if (!checkFloat(p)) {
+      humanMsg.displayMsg('Please, specify good values (use the "." for decimal)', 1);
+      return;
+    }
+    prices[rt.id]= p;
+  }
+  prices= JSON.stringify(prices);
+  llModPricing(getActivePricing(), {prices: prices}, 
+    function(ses, recs) {
+      humanMsg.displayMsg('Sounds good');
+    },
+    function(ses, err) {
+      humanMsg.displayMsg('Error there: ' + err.message);
+    });
+}
+
+function savePeriodPricing() {
+  var prices= {};
+  var dfrom= $('#pdfrom').val();
+  var dto= $('#pdto').val();
+  for (var i= 0; i< _zakRtypes.length; i++) {
+    var rt= _zakRtypes[i];
+    var p= $('#pprice_' + rt.id);
+    if (!checkFloat(p)) {
+      humanMsg.displayMsg('Please, specify good values (use the "." for decimal)', 1);
+      return;
+    }
+    prices[rt.id + '']= p;
+  }
+  /* we have dfrom,dto,prices */
 }
 
 function getActivePricing() {
