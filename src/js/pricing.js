d188ba098d0f1988d4cc024811c4fbc63414831c
effe <effe@krishna.(none)>
2010-07-24 20:50:13 +0200

    go

--- src/js/pricing.js
+++ src/js/pricing.js
@@ -1,23 +1,260 @@
-function setActivePricing(prid) {
-  localStorage.activePricing= prid;
+_zakRtypes= [];
+
+function mergePeriods(oldp, newp) {
+  var os= oldp['dfrom'];
+  var oe= oldp['dto'];
+  var ns= newp['dfrom'];
+  var ne= newp['dto'];
+
+  var res= [];
+  var no;
+  /* the old period contains the new */
+  if (os <= ns && ne <= oe) { 
+    no= copyObject(oldp);
+    no['dto']= ns - 86400;
+    if (no['dto'] > no['dfrom']) 
+      res.push(copyObject(no));
+    else { 
+      no['del']= 1;
+      res.push(copyObject(no));
+    }
+    no= copyObject(oldp);
+    no['dfrom']= newp['dto'] + 86400;
+    if (no['dto'] > no['dfrom']) {
+      delete no.id;
+      res.push(copyObject(no));
+    }
+    else {
+      no['del']= 1;
+      res.push(copyObject(no));
+    }
+    return res;
+  }
+
+  /* the new period contains the old */
+  if (ns <= os && oe <= ne) {
+    no= copyObject(oldp);
+    no['del']= 1;
+    res.push(no);
+    return res;
+  }
+
+  /* pure intersection */
+  /* old older */
+  if (os < ns) {
+    no= copyObject(oldp);
+    no['dto']= newp['dfrom'] - 86400;
+    if (! no['dto'] > no['dfrom'])
+      no['del']= 1;
+    res.push(no);
+    return res;
+  }
+
+  /* old newest */
+  no= copyObject(oldp);
+  no['dfrom']= newp['dto'] + 86400;
+  if (! no['dto'] > no['dfrom']) 
+    no['del']= 1;
+  res.push(no);
+  return res;
 }
-function getActivePricing() {
+
+function _decompressPricing(prices) {
   try {
-    return localStorage.activePricing;
-  } catch(e) {return false};
+    return JSON.parse(prices);
+  } catch(e) {return -1};
 }
+
+function _getRtPricing(prices, rt) {
+  try {
+    var res= parseFloat(prices[rt+'']);
+    if (!res) return 0.0;
+    return res.toFixed(2);
+  } catch(e) {return 0.0};
+}
+
+function designPricingPeriods() {
+  llLoadPricesPeriods(getActivePricing(),
+    function(ses, recs) {
+      var ppres= '';
+      for (var j= 0; j< recs.rows.length; j++) {
+        var pp= recs.rows.item(j);
+        console.log(pp);
+        var pprices= _decompressPricing(pp.prices);
+        ppres+= '<tr><td>' + strDate(pp.dfrom) + '</td><td>' + strDate(pp.dto) + '</td>';
+        for (var z= 0; z< _zakRtypes.length; z++ ) {
+          ppres+= '<td>' + _getRtPricing(pprices, zt.id) + '</td>';
+        }
+        ppres+= '<td><input type="submit" value="Delete" onclick="delPricingPeriod(' + pp.id + ')"/></td></tr>';
+      }
+      $('#defperiods').empty().html(ppres);
+    });
+
+    var hres= '<th><b>From</b></th><th><b>To</b></th>';
+    var dres= '<td><input type="text" id="pdfrom" style="width:75px"></input></td>';
+    dres+= '<td><input type="text" id="pdto" style="width:75px"></input></td>';
+    for (var z= 0; z< _zakRtypes.length; z++ ) {
+      var zt= _zakRtypes[z];
+      hres+= '<th><b>' + zt.name.substring(0,6) + '</b></th>';
+      dres+= '<td><input type="text" id="pprice_' + zt.id + '" style="width:60px"></input></td>';
+    }
+    dres+= '<td><input type="submit" value="Save" onclick="savePeriodPricing()"></td>';
+    $('#lrtypesname').empty().html(hres + '<th><b>Save</b></th>');
+    $('#lrtypesperiodsname').empty().html(hres + '<th><b>Delete</b></th>');
+    $('#lrtypesinput').empty().html(dres);
+    $('#pdfrom').datepicker({dateFormat: 'dd/mm/yy'});
+    $('#pdto').datepicker({dateFormat: 'dd/mm/yy'});
+}
+
+function designPricing() {
+
+  /* cmb pricing */
+  llLoadPricings(function(ses, recs) {
+    var rres= '';
+    for (var i= 0; i< recs.rows.length; i++) {
+      var p= recs.rows.item(i);
+      rres+= '<option value="' + p.id + '">' + p.name + '</option>';
+    }
+    $('#selplan').empty().html(rres);
+    $('#selplan').val(getActivePricing());
+  }, function(ses, err) {humanMsg.displayMsg('Error there: '+ err.message)});
+
+  /* base pricing */
+  llLoadPricing(getActivePricing(), 
+    function(ses, recs) {
+      var p= recs.rows.item(0);
+      var prices= _decompressPricing(p.prices);
+      var res= '';
+
+      function _gp(rt) {
+        return _getRtPricing(prices, rt);
+      }
+
+      for (var i= 0; i< _zakRtypes.length; i++) {
+        var rtype= _zakRtypes[i + ''];
+        res+= '<tr><td><b>' + rtype.name.substring(0,6) + '</b></td>';
+        res+= '<td><input type="text" id="price_' + rtype.id + '" style="width:75px" value="';
+        res+= _gp(rtype.id) + '"></input>';
+      }
+      $('#defprices').empty().html(res);
+    });
+  designPricingPeriods();
+}
+
+function saveBasePrices() {
+  var prices= {};
+  for (var i= 0; i< _zakRtypes.length; i++) {
+    var rt= _zakRtypes[i];
+    var p= $('#price_' + rt.id).val();
+    if (!checkFloat(p)) {
+      humanMsg.displayMsg('Please, specify good values (use the "." for decimal)', 1);
+      return;
+    }
+    prices[rt.id]= p;
+  }
+  prices= JSON.stringify(prices);
+  llModPricing(getActivePricing(), {prices: prices}, 
+    function(ses, recs) {
+      humanMsg.displayMsg('Sounds good');
+    },
+    function(ses, err) {
+      humanMsg.displayMsg('Error there: ' + err.message);
+    });
+}
+
+function savePeriodPricing() {
+  var prices= {};
+  var dfrom= $('#pdfrom').val();
+  var dto= $('#pdto').val();
+  for (var i= 0; i< _zakRtypes.length; i++) {
+    var rt= _zakRtypes[i];
+    var p= $('#pprice_' + rt.id).val();
+    if (!checkFloat(p)) {
+      humanMsg.displayMsg('Please, specify good values (use the "." for decimal)', 1);
+      return;
+    }
+    prices[rt.id + '']= p;
+  }
+  var params= {};
+  params.dfrom= unixDate(dfrom);
+  params.dto= unixDate(dto);
+  params.prices= JSON.stringify(prices);
+  var ap= getActivePricing();
+  /* we have dfrom,dto,prices */
+  llLoadPricesPeriods(ap,
+    function(ses, recs) {
+      if (recs.rows.length == 0) 
+        var saveprices= [params];
+      else {
+        /* let's check intersections */
+        console.log('Matching intersections');
+        var intersections= new Array();
+        for (var i= 0; i< recs.rows.length; i++) {
+          var ppp= recs.rows.item(i);
+          if (parseInt(ppp.dto) >= parseInt(unixDate(dfrom)) && 
+              parseInt(ppp.dfrom) <= parseInt(unixDate(dto))) {
+            intersections.push(ppp);
+          }
+        }
+        console.log(intersections);
+        if (intersections.length == 0) {
+          console.log('No intersections');
+          var saveprices= [params];
+        } else {
+          console.log('Working on intersections');
+          var res= [params], tres, j;
+          for (var i=0; i<intersections.length;i++) {
+            var pint= intersections[i];
+            tres= mergePeriods(pint, params);
+            for (j=0;j<tres.length;j++) {
+              res.push(tres[j]);
+            }
+          }
+          var saveprices= res;
+        }
+      }
+
+      llNewPricesPeriod(ap, saveprices,
+        function(ses, recs) {
+          humanMsg.displayMsg('Sounds good');
+          designPricing();
+          return;
+        },
+        function(ses, err) {
+          humanMsg.displayMsg('Error there: ' + err.message, 1);
+        });
+      return;
+    });
+}
+
+function delPricingPeriod(pid) {
+  llDelPricesPeriod(pid,
+    function(ses, recs) {
+      designPricingPeriods();
+      humanMsg.displayMsg('Sounds good');
+    },
+    function(ses, err) {
+      humanMsg.displayMsg('Error there: ' + err.message, 1);
+    });
+}
+
+function getActivePricing() {
+  return localStorage.activePricing || '';
+}
+
+function setActivePricing(pid) {
+  localStorage.activePricing= pid;
+}
+
 function askNewPricing() {
   $('#newpricing').modal({onClose: function() {
-    console.log('Closing now');
-    console.log(getActivePricing());
     if (!getActivePricing()) {
-      console.log('No Active Pricing: ' + getActivePricing());
       askNewPricing();
       }
   }});
 }
 
-function addRate() {
+function addPricing() {
   var rname= $('#newpricing_name').val();
   if (!rname) {
     humanMsg.displayMsg('Please, specify a good value');
@@ -25,269 +262,43 @@ function addRate() {
   }
   llNewPricing(rname, 
     function(ses, recs) {
-      console.log('Setting active pricing...');
       setActivePricing(recs.insertId);
-      console.log('Closing modal...');
       $.modal.close();
       $.modal.close();
-      console.log('Init pricing');
-      initPricing();
+      designPricing();
     },
     function(ses, err) {
       humanMsg.displayMsg('Error: ' + err.message);
     });
-}
-
-
-var iPricing= function(pricing) {
-  this['pricing']= pricing;
-  this['periods']= new Array();
-
-  this['designPrice']= function(price) {
-    var sdfrom= strDate(price['dfrom']);
-    var sdto= strDate(price['dto']);
-    var res= '<td>' + sdfrom + '</td>';
-    res+= '<td>' + sdto + '</td>';
-    res+= '<td>' + price['price_ro'] + '</td>';
-    res+= '<td>' + price['price_bb'] + '</td>';
-    res+= '<td>' + price['price_hb'] + '</td>';
-    res+= '<td>' + price['price_fb'] + '</td>';
-    res+= '<td><a href="javascript:delPricingPeriod(' + price['id'] + ')">Delete</a></td>';
-    return '<tr>' + res + '</tr>';
-  }
-
-  this['designPrices']= function() {
-    res= '';
-    for (i=0;i<zakPricing.periods.length;i++) {
-      res+= zakPricing.designPrice(zakPricing.periods[i]);
-    }
-    return res;
-  }
-
-  this['designPeriods']= function() {
-    var ap= getActivePricing();
-    llLoadPricesPeriods(ap,
-      function(ses, recs) {
-        zakPricing.periods= arrayFromRecords(recs);
-        $('#defperiods').empty().html(zakPricing.designPrices());
-      },
-      function(ses, err) {
-        humanMsg.displayMsg('Error: ' + err.message);
-      });
-  }
-  this['designMe']= function() {
-    var i, res= '';
-    var ap= getActivePricing(), p;
-    for(i=0;i<zakPricing.pricing.length;i++) {
-      p= zakPricing.pricing[i];
-      if (p['id'] == ap) {
-        res+= '<option selected="selected" value="' + p['id'] + '">' + p['name'] + '</option>';
-        $('#price_ro').val(p['price_ro'] || '');
-        $('#price_bb').val(p['price_bb'] || '');
-        $('#price_fb').val(p['price_fb'] || '');
-        $('#price_hb').val(p['price_hb'] || '');
-      }
-      else
-        res+= '<option value="' + p['id'] + '">' + p['name'] + '</option>';
-    }
-    $('#selplan').empty().html(res);
-    zakPricing.designPeriods();
-  }
-
-  this['mergePeriods']= function(oldp, newp) {
-    var os= oldp['dfrom'];
-    var oe= oldp['dto'];
-    var ns= newp['dfrom'];
-    var ne= newp['dto'];
-
-    var res= [];
-    var no;
-    /* the old period contains the new */
-    if (os <= ns && ne <= oe) { 
-      no= copyObject(oldp);
-      no['dto']= ns - 86400;
-      if (no['dto'] > no['dfrom']) 
-        res.push(copyObject(no));
-      else { 
-        no['del']= 1;
-        res.push(copyObject(no));
-      }
-      no= copyObject(oldp);
-      no['dfrom']= newp['dto'] + 86400;
-      if (no['dto'] > no['dfrom']) {
-        delete no.id;
-        console.log('Pushing');
-        console.log(no);
-        res.push(copyObject(no));
-      }
-      else {
-        no['del']= 1;
-        res.push(copyObject(no));
-      }
-      console.log('Res Res');
-      console.log(res);
-      return res;
-    }
-
-    /* the new period contains the old */
-    if (ns <= os && oe <= ne) {
-      no= copyObject(oldp);
-      no['del']= 1;
-      res.push(no);
-      return res;
-    }
-
-    /* pure intersection */
-    /* old older */
-    if (os < ns) {
-      no= copyObject(oldp);
-      no['dto']= newp['dfrom'] - 86400;
-      if (! no['dto'] > no['dfrom'])
-        no['del']= 1;
-      res.push(no);
-      return res;
-    }
-
-    /* old newest */
-    console.log('Old newest');
-    no= copyObject(oldp);
-    no['dfrom']= newp['dto'] + 86400;
-    if (! no['dto'] > no['dfrom']) 
-      no['del']= 1;
-    res.push(no);
-    return res;
-  }
-
-  this['eatPrices']= function(prices) {
-    if (zakPricing.periods.length == 0) return [prices];
-    var dfrom= prices['dfrom'];
-    var dto= prices['dto'];
-    var intersections= new Array();
-    var i, pp;
-    for(i=0;i<zakPricing.periods.length;i++) {
-      pp= zakPricing.periods[i];
-      if (pp['dto'] >= dfrom && pp['dfrom'] <= dto) 
-        intersections.push(pp);
-    }
-    if (intersections.length == 0) return [prices];
-    var res= [prices], tres, j;
-    for(i=0;i<intersections.length;i++) {
-      pp= intersections[i];
-      tres= zakPricing.mergePeriods(pp, prices);
-      for (j=0;j<tres.length;j++) {
-        console.log(tres[j]['id']);
-        console.log(tres[j]['del']);
-        console.log(jsDate(tres[j]['dfrom']));
-        console.log(jsDate(tres[j]['dto']));
-        res.push(tres[j]);
-      }
-    }
-    console.log(res);
-    return res;
-  }
-
-  return this;
 }
 
 function changeActivePricing() {
   var splan= $('#selplan').val();
   if (splan == getActivePricing()) return;
   setActivePricing(splan);
-  initPricing();
-}
-
-function saveBasePrices() {
-  var pro= $('#price_ro').val();
-  var pbb= $('#price_bb').val();
-  var pfb= $('#price_fb').val();
-  var phb= $('#price_hb').val();
-  var prices= {price_ro: pro, price_fb: pfb, price_hb: phb, price_bb: pbb};
-  for (var k in prices) {
-    if (!checkFloat(prices[k])) {
-      humanMsg.displayMsg('Please, specify valid values', 1);
-      return;
-    }
-  }
-  console.log('Now inserting');
-  llModPricing(getActivePricing(), prices,
-    function(ses, recs) {
-      humanMsg.displayMsg('Sounds good');
-    },
-    function(ses, err) {
-      humanMsg.displayMsg('Error: '+ err.message, 1);
-    });
-}
-
-function delPricingPeriod(ppid) {
-  llDelPricesPeriod(ppid,
-    function(ses, recs) {
-      humanMsg.displayMsg('Sounds good');
-      zakPricing.designPeriods();
-    },
-    function(ses, err) {
-      humanMsg.displayMsg('Error: ' + err.message);
-    });
-}
-
-function addPricingPeriod() {
-  var pro= $('#pro').val();
-  var pbb= $('#pbb').val();
-  var pfb= $('#pfb').val();
-  var phb= $('#phb').val();
-  var prices= {price_ro: pro, price_fb: pfb, price_hb: phb, price_bb: pbb};
-  for (var k in prices) {
-    if (!checkFloat(prices[k])) {
-      humanMsg.displayMsg('Please, specify valid values', 1);
-      return;
-    }
-  }
-  var pdfrom= $('#pdfrom').val();
-  var pdto= $('#pdto').val();
-  var today= unixDate();
-  pdfrom= unixDate(pdfrom);
-  pdto= unixDate(pdto);
-  if (pdfrom > pdto || pdfrom < today || pdto < today) {
-    humanMsg.displayMsg('Please, specify valid dates', 1);
-    return;
-  }
-  prices['dfrom']= pdfrom;
-  prices['dto']= pdto;
-
-  lprices= zakPricing.eatPrices(prices);
-  /*lprices= [prices];*/
-
-  llNewPricesPeriod(getActivePricing(), lprices,
-    function(ses, recs) {
-      humanMsg.displayMsg('Sounds good');
-      zakPricing.designPeriods();
-    },
-    function(ses, err) {
-      humanMsg.displayMsg('Error: ' + err.message);
-    });
-}
-
-function initPricing() {
-  llLoadPricing(
-    function(ses, recs) {
-      if (recs.rows.length == 0) {
-        console.log('No pricing!! Ask new');
-        askNewPricing();
-        return;
-      }
-      if (!getActivePricing()) {
-        setActivePricing(recs.rows.item(0)['id']);
-      }
-      zakPricing= new iPricing(arrayFromRecords(recs));
-      zakPricing.designMe();
-    },
-    function(ses, err) {
-      humanMsg.displayMsg('Error ' + err.message, 1);
-    });
-
+  designPricing();
 }
 
 $(document).ready(function() {
-  $('#pdfrom').datepicker({dateFormat: 'dd/mm/yy'});
-  $('#pdto').datepicker({dateFormat: 'dd/mm/yy'});
-  initPricing();
+  llGetRoomTypes(function(ses, recs) {
+    for (var i= 0; i< recs.rows.length; i++) {
+      var rt= recs.rows.item(i);
+      _zakRtypes.push(rt);
+    }
+    var ap= getActivePricing();
+    if (!ap) {
+      llLoadPricings(function(ses, recs) {
+        if (recs.rows.length != 0) {
+          setActivePricing(recs.rows.item(0));
+          designPricing();
+          return;
+        }
+        askNewPricing();
+        return;
+      });
+    }
+    else designPricing();
+  }, function(ses, err) {
+    humanMsg.displayMsg('Error there: ' + err.message);
+  });
 });
