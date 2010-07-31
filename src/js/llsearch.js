0e41c019f447cf9bc3356f618cc537343e289141
effe <effe@krishna.(none)>
2010-07-31 03:39:25 +0200

    go

--- src/js/llsearch.js
+++ src/js/llsearch.js
@@ -13,3 +13,167 @@ function llSearchCustomers(s, cb, cbe) {
       _likeZakSearch([s,s]), cb, cbe);
   });
 }
+
+function _stupidSearch(s, t, cb, cbe) {
+  var db= zakOpenDb();
+  db.readTransaction(function(ses) {
+    ses.executeSql('select * from ' + t + ' where name like ?', 
+      _likeZakSearch([s]), cb, cbe);
+  });
+}
+
+function _stupidDelete(i,t,cb,cbe) {
+  var db= zakOpenDb();
+  db.transaction(function(ses) {
+    ses.executeSql('delete from ' + t + ' where id = ?', [i], 
+    cb || function(ses, recs) {
+      $.modal.close();
+      /*document.location.reload(false);*/
+      humanMsg.displayMsg('Sounds good');
+    }, cbe ||
+    function(ses, err) {
+      $.modal.close();
+      humanMsg.displayMsg('Error there: ' + err.message, 1);
+    });
+  });
+}
+
+function llSearchExtras(s, cb, cbe) {
+  _stupidSearch(s, 'extra', cb, cbe);
+}
+
+function llSearchPricing(s, cb, cbe) {
+  _stupidSearch(s, 'pricing', cb, cbe);
+}
+
+
+function delExtra(eid) {
+  $('#deleting_link').attr('href', 'javascript:_delExtra(' + eid + ')');
+  $('#deleting_div').modal();
+}
+
+function _delExtra(eid) {
+  _stupidDelete(eid, 'extra');
+}
+
+function updateExtra(eid) {
+  var name= $('#e_name_' + eid).val();
+  if (!name) {
+    humanMsg.displayMsg('Please, specify a valid name');
+    return;
+  }
+  var cost= $('#e_cost_' + eid).val();
+  var vat= $('#e_vat_' + eid).val();
+  if (!checkFloat(cost) || !checkFloat(vat)) {
+    humanMsg.displayMsg('Please, specify good values (decimal values? use "." - the-dot -)');
+    return;
+  }
+  var perday= $('#e_perday_' + eid).val();
+  var db= zakOpenDb();
+  db.transaction(function(ses) {
+    var ob= {
+      name: name,
+      cost: cost,
+      vat: vat,
+      perday: perday
+    }
+    var q= updateStatementWhere(ob, 'where id = ?', [eid], 'extra');
+    console.log(q);
+    ses.executeSql(q.qry, q.qarr,
+    function(ses, recs) {
+      $.modal.close();
+      /*document.location.reload(false);*/
+      humanMsg.displayMsg('Sounds good');
+    },
+    function(ses, err) {
+      $.modal.close();
+      humanMsg.displayMsg('Error there: ' + err.message, 1);
+    });
+  });
+}
+
+function delPricing(pid) {
+  $('#deleting_link').attr('href', 'javascript:_delPricing(' + pid + ')');
+  $('#deleting_div').modal();
+}
+
+function _delPricing(pid) {
+  _stupidDelete(pid, 'pricing');
+}
+
+function updatePricing(pid) {
+  var name= $('#p_name_' + pid).val();
+  var db= zakOpenDb();
+  db.transaction(function(ses) {
+    ses.executeSql('update pricing set name = ? where id = ?', [name, pid],
+    function(ses, recs) {
+      $.modal.close();
+      /*document.location.reload(false);*/
+      humanMsg.displayMsg('Sounds good');
+    },
+    function(ses, err) {
+      $.modal.close();
+      humanMsg.displayMsg('Error there: ' + err.message, 1);
+    });
+  });
+}
+
+function delCustomer(cid) {
+  $('#delcustomer_id').val(cid);
+  $('#delcustomer_div').modal();
+}
+
+function _delCustomer() {
+  var cid= $('#delcustomer_id').val();
+  console.log('Deleting customer:' + cid);
+  llDelCustomer(cid, false,
+    function(ses, recs) {
+      $.modal.close();
+      /*document.location.reload(false);*/
+      humanMsg.displayMsg('Sounds good');
+    },
+    function(ses, err) {
+      $.modal.close();
+      humanMsg.displayMsg('Error there: ' + err.message, 1);
+    });
+}
+function updateCustomer(cid) {
+  var _i= function(n) {
+    return $('#c_' + n + '_' + cid).val();
+  }
+  console.log('Updating customer');
+  var name= _i('name');
+  if (!name) {
+    humanMsg.displayMsg('Please, specify a valid customer name');
+    return;
+  }
+  var mail= _i('email') || '';
+  var phone= _i('phone') || '';
+  var city= _i('city') || '';
+  var zip= _i('zip') || '';
+  var address= _i('address') || '';
+  var bmonth= _i('bmonth') || 1;
+  var byear= _i('byear') || '';
+  var bplace= _i('bplace') || '';
+  var bnotes= _i('notes') || '';
+  var gender= _i('gender') || 1;
+  var d= {
+    name: name,
+    email: mail,
+    phone: phone,
+    zip: zip,
+    address: address,
+    bmonth: bmonth,
+    byear: byear,
+    bplace: bplace,
+    notes: bnotes,
+    gender: gender
+  }
+  llModCustomer(cid, d, false, false, 
+    function(ses, recs) {
+      humanMsg.displayMsg('Sounds good');
+    },
+    function(ses, err) {
+      humanMsg.displayMsg('Error there: ' + err.message, 1);
+    });
+}
--- src/js/search.js
+++ src/js/search.js
@@ -12,6 +12,33 @@ zakLookingResultsN= 0;
 zakSearchCounter= 0;
 zakSearchedNow= '';
 
+function _i(iid, ivl, attrs) {
+  var res= '<input type="text" id="' + iid + '" value="' + ivl + '"';
+  if (attrs) res+= ' ' + attrs + ' ';
+  res+= '></input>';
+  return res;
+}
+
+function _cmbMonths(iid) {
+  return '<select id="' + iid + '">' + 
+    '<option value="1">Jan</option>' + 
+    '<option value="2">Feb</option>' + 
+    '<option value="3">Mar</option>' + 
+    '<option value="4">Apr</option>' + 
+    '<option value="5">May</option>' + 
+    '<option value="6">Jun</option>' + 
+    '<option value="7">Jul</option>' + 
+    '<option value="8">Aug</option>' + 
+    '<option value="9">Sep</option>' + 
+    '<option value="10">Oct</option>' +
+    '<option value="11">Nov</option>' +
+    '<option value="12">Dec</option></select>';
+}
+var _cmbGender= function(iid, m) {
+    if (m==1) return '<select id="' + iid + '"><option value="1" selected="selected">Male</option><option value="2">Female</option></select>';
+    return '<select id="' + iid + '"><option value="1">Male</option><option selected="selected" value="2">Female</option></select>';
+}
+
 function putSearchResult(s, rec) {
   zakSearchCounter+= 1;
   if (!zakLookResults[s]) 
@@ -19,37 +46,89 @@ function putSearchResult(s, rec) {
   else 
     zakLookResults[s].push(rec);
 
+  var afterF= [];
   var dis= "javascript:$('#div_search_" + zakSearchCounter + "').toggle()";
   var res= '';
+  var _preamble= function(i, n, fdel) {
+    var r= '<div class="div_search"><a href="' + dis + '"><img src="' + i + '"></img></a> <b>' + n + '</b>';
+    r+= '<div class="div_search_content" style="display:none" id="div_search_' + zakSearchCounter + '">';
+    if (fdel) {
+      r+= '<div style="float:right">';
+      r+= '<input type="submit" value="Delete" onclick="' + fdel + '(' + rec.id + ')"></input>';
+      r+= '</div>';
+    }
+    return r;
+  };
+
+  if (rec.fromtable == 'extra') {
+    res+= _preamble('/imgs/extra.png', rec.name, 'delExtra');
+    res+= '<b>' + rec.name + '</b>';
+    res+= '<table><tr>';
+    res+= '<td>Name</td>';
+    res+= '<td><input type="text" value="' + rec.name + '" id="e_name_' + rec.id + '"></input></td></tr>';
+    res+= '<tr><td>Cost</td>';
+    res+= '<td><input style="width:60px" type="text" value="' + rec.cost + '" id="e_cost_' + rec.id + '"></input></td></tr>';
+    res+= '<tr><td>Vat</td>';
+    res+= '<td><input style="width:40px" type="text" value="' + rec.vat + '" id="e_vat_' + rec.id + '"></input></td></tr>';
+    res+= '<tr><td>Per day</td>';
+    res+= '<td><select id="e_perday_' + rec.id + '">';
+    if (rec.perday) 
+      res+= '<option value="1" selected="selected">Yes</option><option value="0">No</option></select>';
+    else
+      res+= '<option value="1">Yes</option><option selected="selected" value="0">No</option></select>';
+    res+= '</td></tr>';
+    res+= '<tr><td colspan="2" align="center"><input type="submit" value="Update Extra" onclick="updateExtra(' + rec.id + ')"></input></td></tr>';
+    res+= '</table>';
+    res+= '</div>';
+  }
+
+  if (rec.fromtable == 'pricing') {
+    res+= _preamble('/imgs/pricing.png', rec.name, 'delPricing');
+    res+= '<b>' + rec.name + '</b>';
+    res+= '<table><tr>';
+    res+= '<td>Name</td>';
+    res+= '<td><input type="text" value="' + rec.name + '" id="p_name_' + rec.id + '"></input></td></tr>';
+    res+= '<tr><td colspan="2" align="center"><input type="submit" value="Update Name" onclick="updatePricing(' + rec.id + ')"></input></td></tr>';
+    res+= '</table>';
+    res+= '</div>';
+  }
+
   if (rec.fromtable == 'customer') {
     if (rec.gender == 1)
       var isrc= '/imgs/male.png';
     else
       var isrc= '/imgs/female.png';
-    var img= '<a href="' + dis + '"><img style="margin-top:4px" src="' + isrc + '"></img></a> ';
-    res+= '<div class="div_search">';
-    res+= '<b>' + img + rec.name + '</b> (' + (rec.email || 'Unknown mail') + ')';
-    res+= '<div class="div_search_content" style="display:none" id="div_search_' + zakSearchCounter + '">';
-    res+= '<div style="float:right">';
-    /*res+= '<a href="newReservation(' + rec.id + ')">Add reservation</a> ';*/
-    res+= '<a href="editCustomer(' + rec.id + ')">Edit</a> ';
-    res+= '<a href="delCustomer(' + rec.id + ')">Delete</a> ';
-    res+= '</div>';
-    res+= rec.name;
+    var rid= rec.id;
+    if (rec.email)
+      res+= _preamble(isrc, '<b>' + rec.name + '</b> (' + rec.email + ')', 'delCustomer');
+    else
+      res+= _preamble(isrc, '<b>' + rec.name + '</b>', 'delCustomer');
+    var _ii= function(n, attrs) {
+      return _i('c_' + n + '_' + rid, rec[n], attrs);
+    }
+    if (rec.country_code)
+      res+= '<img src="/imgs/flags/' + rec.country_code.toLowerCase() + '.gif"></img> ' + rec.name;
+    else
+      res+= rec.name;
     res+= '<table>';
-    res+= '<tr><td>Mail:</td><td>' + rec.email + '</td></tr>';
-    res+= '<tr><td>Phone:</td><td>' + rec.phone + '</td></tr>';
-    res+= '<tr><td>Country:</td><td>' + rec.country + '</td></tr>';
-    res+= '<tr><td>City:</td><td>' + rec.city + '</td></tr>';
-    res+= '<tr><td>Zip:</td><td>' + rec.zip + '</td></tr>';
-    res+= '<tr><td>Address:</td><td>' + rec.address + '</td></tr>';
-    res+= '<tr><td>Birth:</td><td>' + rec.bmonth + '/' + rec.byear + '</td></tr>';
-    res+= '<tr><td>Remarks:</td><td>' + rec.notes + '</td></tr>';
-    res+= '</table>';
+    res+= '<tr><td>Name:</td><td>' + _ii('name') +'</td>' + '<td colspan="2">' + _cmbGender('c_gender_' + rid, rec.gender) + '</td></tr>';
+    res+= '<tr><td>Mail:</td><td>' + _ii('email') + '</td>';
+    res+= '<td>Phone:</td><td>' + _ii('phone') + '</td></tr>';
+    res+= '<tr><td>City:</td><td>' + _ii('city') + '</td>';
+    res+= '<td>Zip:</td><td>' + _ii('zip') + '</td></tr>';
+    res+= '<tr><td>Address:</td><td>' + _ii('address') + '</td></tr>';
+    res+= '<tr>';
+    res+= '<td>Birth pl.:</td><td>' + _ii('bplace') + '</td>';
+    res+= '<td>Birth:</td><td>' + _cmbMonths('c_bmonth_' + rid) + _ii('byear', 'style="width:60px"')+'</td>';
+    res+= '</tr>';
+    res+= '<tr><td>Remarks:</td><td colspan="3"><textarea style="width:100%;height:80px" id="c_notes_' + rid + '">' + rec.notes + '</textarea></td></tr>';
+    res+= '<tr><td align="center" colspan="2"><input onclick="updateCustomer(' + rid + ')" type="submit" value="Update customer"></input></td></tr></table>';
     res+= '</div></div>';
+    afterF.push(function() {$('#c_month_' + rid).val(rec.bmonth);});
   }
 
   $('#zakResults').append(res);
+  for (var i= 0; i< afterF.length; i++) afterF[i]();
 }
 
 function afterLook(s) {
