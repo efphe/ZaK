9b21b206bf7afb3548f1b4e971ca71760ee75a15
yellow <yellow@wubook.net>
2010-09-08 21:52:25 +0200

    zakCheckMail() compiled

--- src/js/commons.js
+++ src/js/commons.js
@@ -282,6 +282,7 @@ function getCurrency() {
   return cur;
 }
 
-function zakCheckMail(m) {
-  return true;
+function zakCheckMail(email) {
+  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;  
+  return emailPattern.test(email);  
 }
