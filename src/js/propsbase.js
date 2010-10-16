57a6f9c94581fe55007ecfde7b36ca6e0328ec99
yellow <yellow@wubook.net>
2010-10-16 21:18:51 +0200

    Adding icon to "Add reservation on the fly"

--- src/js/propsbase.js
+++ src/js/propsbase.js
@@ -21,6 +21,86 @@ function setActiveProperty(prop, cbs, cbe) {
       });
 }
 
+_modalNewRes= '' + 
+' <div id="addNewFlyReservation" style="display:none" class="zakmodal">'+
+'<h1>Add a new reservation</h1>'+
+'<h2>Please, insert reservation data</h2>'+
+'<table>'+
+'<tr><td>Arrival:</td><td>'+
+'<input type="text" style="width:150px" id="flyReservationWhen"></input></td></tr>'+
+'<tr>'+
+'<td>'+
+'Nights:'+
+'</td>'+
+'<td>'+
+'<select id="addNewFlyReservationNightsCmb">'+
+'<option value="z" selected="selected">--</option>'+
+'<option value="1">1</option>'+
+'<option value="2">2</option>'+
+'<option value="3">3</option>'+
+'<option value="4">4</option>'+
+'<option value="5">5</option>'+
+'<option value="6">6</option>'+
+'<option value="7">7</option>'+
+'<option value="8">8</option>'+
+'<option value="9">9</option>'+
+'<option value="10">10</option>'+
+'<option value="11">11</option>'+
+'<option value="12">12</option>'+
+'<option value="13">13</option>'+
+'<option value="14">14</option>'+
+'<option value="15">15</option>'+
+'</select>'+
+'</td>'+
+'</tr>'+
+'<tr>'+
+'<td>'+
+'          Customer'+
+'</td>'+
+'<td>'+
+'<input type="text" id="addNewFlyReservationCustomer"></input>'+
+'</td>'+
+'</tr>'+
+'<tr>'+
+'<td>'+
+'          Status:'+
+'</td>'+
+'<td>'+
+'<select id="addNewFlyReservationStatus">'+
+'<option value="1" selected="selected">Confirmed</option>'+
+'<option value="2">Not confirmed</option>'+
+'<option value="3">Checkin\'ed</option>'+
+'<option value="4">Option</option>'+
+'</select>'+
+'</td>'+
+'</tr>'+
+'<tr>'+
+'<td>'+
+'          Go to details:'+
+'</td>'+
+'<td>'+
+'<input type="checkbox" id="addNewFlyReservationDetails" checked="checked"></input>'+
+'</td>'+
+'</tr>'+
+'</table>'+
+'<div style="float:right">'+
+'<a style="color:#36ff00;font-weight:bold" href="javascript:_addNewReservation()">Continue</a> '+
+'<a style="color:red;font-weight:bold" href="javascript:$.modal.close()">Cancel</a>'+
+'</div>'+
+'<br/>'+
+'</div>';
+
+
+function flyReservation() {
+  var fr= $('#addNewFlyReservation');
+  if (fr.length == 0) {
+    $('body').append(_modalNewRes);
+    return flyReservation();
+  }
+  $('#flyReservationWhen').datepicker();
+  $('#addNewFlyReservation').modal();
+}
+
 function getActiveProperty() {
   try {
     return JSON.parse(localStorage.activeProperty);
